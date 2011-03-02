fbb789ec032240c24ba8b4f0065a33e87d363bc6
Matt Gardner <mjg82@byu.edu>
2011-03-01 21:20:45 -0700

    Added some stuff for /etc, and updated my trac notes

---
+++ etc/cron.hourly/backup_databases.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cp /home/mjg82/studyjournal/db /home/mjg82/studyjournal/db.backup
+cp /home/mjg82/language_study/db /home/mjg82/language_study/db.backup
+
+chown mjg82:aml /home/mjg82/studyjournal/db.backup
+chown mjg82:aml /home/mjg82/language_study/db.backup
+
