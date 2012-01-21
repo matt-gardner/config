122fe6d94bc524abb106662030def786f8bc5912
Matt Gardner <mjg82@byu.edu>
2012-01-20 19:05:35 -0500

    Some path changes to reflect running this on my home machine instead of aml

--- etc/cron.daily/backup_databases.sh
+++ etc/cron.daily/backup_databases.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cp /home/mjg82/studyjournal/db /home/mjg82/backups/studyjournal.db.backup
-cp /home/mjg82/language_study/db /home/mjg82/backups/language.db.backup
+cp /home/matt/studyjournal/db /media/backup_disk/databases/studyjournal.db
+cp /home/matt/language_study/db /media/backup_disk/databases/language.db
 
-chown mjg82:aml /home/mjg82/backups/studyjournal.db.backup
-chown mjg82:aml /home/mjg82/backups/language.db.backup
+chown matt:matt /media/backup_disk/databases/studyjournal.db
+chown matt:matt /media/backup_disk/databases/language.db
 
