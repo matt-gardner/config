2a9934c2c7bf7f3fa064852a7381929246bf4b6c
Matt Gardner <mattg@allenai.org>
2016-04-05 00:13:30 +0000

    Some improvements from my ec2 machine

--- ec2_start_byobu.sh
+++ ec2_start_byobu.sh
@@ -5,8 +5,12 @@ tmux start-server
 # $2 is the name of the window
 # $3 is the window index
 createDevWindow() {
-  byobu-tmux new-window -dP -c $1 -k -t $3 -n $2
-  byobu-tmux split-window -d -c $1 -h -l 434 -t dev:$2
+  if [[ "$4" == "sbt" ]]; then
+    byobu-tmux new-window -dP -c $1 -k -t $3 -n $2 "bash --init-file <(echo '. ~/.bashrc && sbt')"
+  else
+    byobu-tmux new-window -dP -c $1 -k -t $3 -n $2
+  fi
+  byobu-tmux split-window -d -c $1 -h -l 434 -t dev:$2 "vim build.sbt"
   byobu-tmux split-window -d -c $1 -l 15 -t dev:${2}.0
 }
 
@@ -29,11 +33,13 @@ createWindowWithCommand() {
 
 byobu-tmux new-session -d -s dev
 
-createDevWindow ~/clone/tacl2015-factorization semparse 1
-createDevWindow ~/clone/jklol jklol 2
-createDevWindow ~/clone/pra pra 3
-createDevWindow ~/clone/util util 4
+createDevWindow ~/clone/semparse semparse 1 sbt
+createDevWindow ~/clone/pra pra 2 no_sbt
+createDevWindow ~/clone/util util 3 no_sbt
+createDevWindow ~/clone/jklol jklol 4 no_sbt
 
 createBlankWindow ~/ misc 5
 
 createWindowWithCommand ~/clone/eclipse eclim 9 "(Xvfb :1 -screen 0 1024x768x24 &);  DISPLAY=:1 ./eclimd"
+
+byobu attach
