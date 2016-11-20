49f671225d8ca0dfc111f4eca4828fcd42e68ad6
Matt Gardner <mattg@allenai.org>
2016-11-20 23:32:52 +0000

    Some improvements from setting up a new EC2 machine

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ dotfiles=(
   byobu
   gitconfig
   gitignore
+  sbt
   shrc
   toprc
   vim
@@ -15,28 +16,71 @@ dotfiles=(
 
 for dotfile in "${dotfiles[@]}"
 do
-  echo "Linking $dotfile with: ln -sf $HOME/clone/config/dotfiles/$dotfile .$dotfile"
+  printf "Linking $dotfile with: ln -sf $HOME/clone/config/dotfiles/$dotfile .$dotfile\n"
   ln -sf $HOME/clone/config/dotfiles/$dotfile .$dotfile
 done
 
-echo "Installing vundle with: git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim"
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+if [ ! -d $HOME/.vim/bundle/Vundle.vim ];
+then
+  printf "\nInstalling vundle with: git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim\n"
+  git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
-echo "Installing vundle plugins with: vim -c PluginInstall -c quitall"
-vim -c PluginInstall -c quitall
+  printf "Installing vundle plugins with: vim -c PluginInstall -c quitall\n"
+  vim -c PluginInstall -c quitall
 
-if hash apt-get 2>/dev/null; then
-  echo "Trying to install cmake and python-dev with: sudo apt-get install cmake python-dev"
-  sudo apt-get install -y cmake python-dev
+  if hash apt-get 2>/dev/null; then
+    printf "Trying to install YCM pre-requisites with apt-get\n"
+    sudo apt-get install -y build-essential cmake python-dev python3-dev
+  else
+    printf "Trying to install YCM pre-requisites with dnf\n"
+    sudo dnf install build-essential cmake python-dev python3-dev
+  fi
+
+  printf "Compiling YCM\n"
+  cd ~/.vim/bundle/YouCompleteMe
+  ./install.py
+  cd -
 else
-  echo "Trying to install cmake and python-dev with: sudo dnf install cmake python-dev"
-  sudo dnf install cmake python-dev
+  printf "\nLooks like you already installed vundle; skipping vim setup steps\n"
 fi
 
-echo "Compiling YCM"
-cd ~/.vim/bundle/YouCompleteMe
-./install.py
-cd -
+if [ ! -d $HOME/anaconda3 ];
+then
+  printf "\nInstalling anaconda\n"
+  wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
+  bash Anaconda3-4.2.0-Linux-x86_64.sh
+  rm Anaconda3-4.2.0-Linux-x86_64.sh
+else
+  printf "\nLooks like you've already installed anaconda3; skipping python setup steps\n"
+fi
 
-echo "TODO: set up eclim"
-echo "TODO: set up sbt plugins (or add some of .sbt to the dotfiles)"
+ec2_scripts=(
+  ec2_start_byobu.sh
+  create_dev_window.sh
+)
+
+if [[ `hostname` == *ai2 ]];
+then
+  printf "\nSetting up AI2-specific EC2 instance stuff\n"
+
+  printf "\nMoving .bashrc to .bash_user\n"
+  mv .bashrc .bash_user
+
+  for script in "${ec2_scripts[@]}"
+  do
+    printf "Linking $script with: ln -sf $HOME/clone/config/$script\n"
+    ln -sf $HOME/clone/config/$script
+  done
+
+  printf "\nInstalling some EC2-specific packages\n"
+  sudo apt-get install nfs-client
+
+  printf "\nAdding the EFS volume to /etc/fstab (and mounting it now)\n"
+  sudo sh -c "echo us-west-2b.fs-07679aae.efs.us-west-2.amazonaws.com:/ /efs nfs4 nfsvers=4.1 >> /etc/fstab"
+  sudo mount us-west-2b.fs-07679aae.efs.us-west-2.amazonaws.com:/ /efs -t nfs4
+
+else
+  printf "\nNot an AI2 EC2 instance; skipping EC2-specific stuff\n"
+fi
+
+printf "TODO: set up eclim\n"
