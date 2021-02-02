b4ddc267bb08b8d11a3ae722ab0b542fb4f12800
Matt Gardner <mattg@allenai.org>
2021-02-02 15:18:39 -0800

    updates from setting up new work laptop

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@ dotfiles=(
   byobu
   gitconfig
   gitignore
-  sbt
   shrc
   toprc
   vim
@@ -31,56 +30,25 @@ then
   if hash apt-get 2>/dev/null; then
     printf "Trying to install YCM pre-requisites with apt-get\n"
     sudo apt-get install -y build-essential cmake python-dev python3-dev exuberant-ctags
+    printf "Compiling YCM\n"
+    cd ~/.vim/bundle/YouCompleteMe
+    ./install.py
+    cd -
   else
-    printf "Trying to install YCM pre-requisites with dnf\n"
-    sudo dnf install build-essential cmake python-dev python3-dev
+    printf "You need to install pre-requisites for YCM and compile it yourself\n"
   fi
 
-  printf "Compiling YCM\n"
-  cd ~/.vim/bundle/YouCompleteMe
-  ./install.py
-  cd -
 else
   printf "\nLooks like you already installed vundle; skipping vim setup steps\n"
 fi
 
-if [ ! -d $HOME/anaconda3 ];
-then
-  printf "\nInstalling anaconda\n"
-  wget https://repo.continuum.io/archive/Anaconda3-5.1.0-Linux-x86_64.sh
-  bash Anaconda3-5.1.0-Linux-x86_64.sh
-  rm Anaconda3-5.1.0-Linux-x86_64.sh
-else
-  printf "\nLooks like you've already installed anaconda3; skipping python setup steps\n"
-fi
-
-ec2_scripts=(
-  ec2_start_byobu.sh
+scripts=(
+  start_byobu.sh
   create_dev_window.sh
 )
 
-if [[ `hostname` == *ai2 ]];
-then
-  printf "\nSetting up AI2-specific EC2 instance stuff\n"
-
-  printf "\nMoving .bashrc to .bash_user\n"
-  mv .bashrc .bash_user
-
-  for script in "${ec2_scripts[@]}"
-  do
-    printf "Linking $script with: ln -sf $HOME/clone/config/$script\n"
-    ln -sf $HOME/clone/config/$script
-  done
-
-  printf "\nInstalling some EC2-specific packages\n"
-  sudo apt-get install nfs-client
-
-  printf "\nAdding the EFS volume to /etc/fstab (and mounting it now)\n"
-  sudo sh -c "echo us-west-2b.fs-07679aae.efs.us-west-2.amazonaws.com:/ /efs nfs4 nfsvers=4.1 >> /etc/fstab"
-  sudo mount us-west-2b.fs-07679aae.efs.us-west-2.amazonaws.com:/ /efs -t nfs4
-
-else
-  printf "\nNot an AI2 EC2 instance; skipping EC2-specific stuff\n"
-fi
-
-printf "TODO: set up eclim\n"
+for script in "${scripts[@]}"
+do
+  printf "Linking $script with: ln -sf $HOME/clone/config/$script\n"
+  ln -sf $HOME/clone/config/$script
+done
